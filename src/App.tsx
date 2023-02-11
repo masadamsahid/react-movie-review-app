6639254b1816dbafd6aac9732e75b8de62e794bd
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-02-12 03:17:30 +0700

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import api from "./api/axiosConfig";
 import {Route, Routes} from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/home/Home";
+import Header from "./components/header/Header";
 
 function App() {
   const [movies, setMovies] = useState<any>();
@@ -29,6 +30,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="/" element={<Home movies={movies}/>}/>
