9045dbf0527c3999418334b09a5ff5e2c2f3bf91
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-02-12 01:10:37 +0700

    get all movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
+import api from "./api/axiosConfig";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState();
+  
+  const getMovies = async () => {
+    
+    try {
+      const response = await api.get("/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+    
+  }
+  
+  useEffect(() => {
+    getMovies();
+  },[]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      App
     </div>
   )
 }
--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/v1",
-  headers: {
-    "ngrok-skip-browser-warning": "true"
-  }
+  baseURL: "http://localhost:8000/api/v1",
+  headers: {"ngrok-skip-browser-warning": "true"}
 });
