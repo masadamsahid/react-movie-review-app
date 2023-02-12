753288e09a640168f60245d7182d0bb081f6c60f
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-02-12 07:11:22 +0700

    set `baseUrl` as a dynamic value retrived from `.env`

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: import.meta.env.VITE_AXIOS_BASE_URL+"/api/v1",
   headers: {"ngrok-skip-browser-warning": "true"}
 });
 
